3248e417666f51876a0f42b63c937ec97e70d02e
Matthew Shepherd <matthewshepherd@Matthews-MBP-2.lan>
2026-08-29 08:45:07 +0100

    removed token capitalisation

--- src/services/assessmentService.ts
+++ src/services/assessmentService.ts
@@ -3,7 +3,7 @@ export async function getAssessment(token: string) {
     console.log("TOKEN=", token);
 
     const response = await fetch(
-        `https://eprom-api-augvdqaeg4h5a9gj.uksouth-01.azurewebsites.net/api/GetAssessment?token=${token.toUpperCase()}`
+        `https://eprom-api-augvdqaeg4h5a9gj.uksouth-01.azurewebsites.net/api/GetAssessment?token=${token}`
     );
 
     if (!response.ok) {
